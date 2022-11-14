a1e2adf448bb30911bc0a03882ecaba58824c4d6
Roberto Llontop <irobertuber@gmail.com>
2022-11-14 17:33:56 -0500

    chore: finish component styles

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "light-gray": "hsl(212, 45%, 89%)",
+        "grayish-blue": "hsl(220, 15%, 55%)",
+        "dark-blue": "hsl(218, 44%, 22%)",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
